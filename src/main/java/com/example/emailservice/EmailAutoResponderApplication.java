c322ec11f2c7b6212bf8aad0edb4b436ada92f7b
Jeyasaravanan18 <sarjeya18@gmail.com>
2025-03-25 22:41:55 +0530

    Initial commit - Email Auto-Responder

---
+++ src/main/java/com/example/emailservice/EmailAutoResponderApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAutoResponderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAutoResponderApplication.class, args);
+	}
+
+}
